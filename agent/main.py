644c8a8089b6af3202434eeede7518471740e156
019-Mahansa <lightningplay2009@gmail.com>
2026-03-08 12:22:09 +0700

    ngetes TinyFish di masukin ke dalam fastApi

--- agent/main.py
+++ agent/main.py
@@ -11,11 +11,33 @@
 # print(response.message.content)
 
 # # test with ollama library
+from dotenv import load_dotenv
+load_dotenv()
 
+from tinyfish import TinyFish
 from fastapi import FastAPI
 
 app = FastAPI()
+client = TinyFish()
 
 @app.get("/")
 def hello():
-  return "hello"
+    return {"message": "hello"}
+
+@app.get("/scrape")
+def scrape(url: str):
+
+    result = None
+
+    with client.agent.stream(
+        url=url,
+        goal="Summarize this website"
+    ) as stream:
+
+        for event in stream:
+            if event.type.value == "COMPLETE":
+                result = event.result_json
+
+    return {"result": result}
+
+#ngetes fastapi di gabung sama tinyfish
