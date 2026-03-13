940c258c998f65f287e23329a8fb131dd5f00646
nijika21 <ganiadriantio@gmail.com>
2026-03-13 21:42:11 +0700

    ya allah gw lupa ngasih hover

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,11 +5,12 @@ export default function Home() {
      <section className="flex justify-center mt-20 py-35">
       <div className="max-w-6xl w-full px-6 justify-center flex flex-col items-center">
         
-        <h1 className="text-4xl font-bold">
+        <div className="px-3 py-1.5 text-[12px] font-bold text-gray-600 bg-amber-50 rounded-[45px]">BLOG</div>
+        <h1 className="text-6xl font-bold">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
 
-        <p className="mt-4 text-gray-600">
+        <p className="mt-4 text-gray-700 font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptas dolor!
         </p>
 
