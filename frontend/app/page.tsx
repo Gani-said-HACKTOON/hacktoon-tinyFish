cf2738fcaa5122c7fb8ab8213cc040452f386265
nijika21 <ganiadriantio@gmail.com>
2026-03-13 21:14:00 +0700

    mobile tablet

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
      <section className="flex justify-center mt-20 py-35">
-      <div className="max-w-6xl w-full px-6 justify-center flex flex-col items-center">
+      <div className="max-w-6xl w-full px -6 justify-center flex flex-col items-center">
         
         <h1 className="text-4xl font-bold">
           Susah bgt ajgggg
