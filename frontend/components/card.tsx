a180f8846bdf63b390acdedc94f80b0d29edd2c3
019-Mahansa <lightningplay2009@gmail.com>
2026-03-19 12:19:03 +0700

    add contact us pages

---
+++ frontend/components/card.tsx
@@ -0,0 +1,10 @@
+export default function Card(){
+    return(
+        <>
+            <div className="card">
+                <span>Name</span>
+                <input type="text" placeholder="Eg. Jane Smith"></input>
+            </div>
+        </>
+    )
+}
